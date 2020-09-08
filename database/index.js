d2cef0405e1f74bec729a9304ba7bf4d9abccfa1
yerx <yer.x.xiong@gmail.com>
2020-09-08 13:16:18 -0400

    changed localhost to mongo to test docker config

--- database/index.js
+++ database/index.js
@@ -1,32 +1,31 @@
-const mongoose = require('mongoose');
-mongoose.Promise = require('bluebird');
-const Experience = require('./experience.js');
+const mongoose = require("mongoose");
+mongoose.Promise = require("bluebird");
+const Experience = require("./experience.js");
 
-mongoose.set('useCreateIndex', true);
+mongoose.set("useCreateIndex", true);
 
 const db = mongoose.connection;
 
-const mongooseConnection = 'mongodb://localhost/fec-airbnb';
+const mongooseConnection = "mongodb://mongo/fec-airbnb";
 
 mongoose
   .connect(mongooseConnection, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Connection error: ', err));
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Connection error: ", err));
 
 const getExperiences = (expId) => {
   const experienceId = expId;
 
   return new Promise((resolve, reject) => {
-    Experience.find({ experienceId })
-      .exec((err, data) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(data);
-      });
+    Experience.find({ experienceId }).exec((err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
   });
 };
 
